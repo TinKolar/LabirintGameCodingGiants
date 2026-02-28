f8aa96379466421057e309ec9c8031dae40ca47a
Wood <68517302+TinKolar@users.noreply.github.com>
2026-02-28 11:26:25 +0100

    Player controller i Camera rotation
    Druge vježbe Coding Giants

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 12f;
+    Vector3 velocity;
+    CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * speed * Time.deltaTime);
+    }
+}
