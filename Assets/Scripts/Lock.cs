79e27a5c62e77b5442cbfe94c6e4dc1f929c759f
Wood <68517302+TinKolar@users.noreply.github.com>
2026-05-09 11:33:29 +0200

    v10 finished

--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -6,19 +6,22 @@ public class Lock : MonoBehaviour
     public Door[] doors;
     public KeyColor myColor;
     bool isLocked=false;
-    public Key Animator;
+    public Animator key;
     
 
     bool iCanOpen=false;
 
     void Start()
     {
-        
+        key = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && iCanOpen == true && !isLocked)
+        {
+            key.SetBool("useKey", CheckKey());
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,6 +39,41 @@ public class Lock : MonoBehaviour
         {
             iCanOpen = false;
             Debug.Log("You can not use this Lock");
+        }
+    }
+
+    public void UseKey()
+    {
+        foreach (var door in doors)
+        {
+            door.Open();
+        }
+    }
+
+    public bool CheckKey()
+    {
+        if (GameManager.Instance.redKey > 0 && myColor == KeyColor.Red)
+        {
+            GameManager.Instance.redKey--;
+            isLocked = true;
+            return true;
+        }
+        else if(GameManager.Instance.greenKey > 0 && myColor == KeyColor.Green)
+        {
+            GameManager.Instance.greenKey--;
+            isLocked = true;
+            return true;
+        }
+        else if(GameManager.Instance.goldKey > 0 && myColor == KeyColor.Gold)
+        {
+            GameManager.Instance.goldKey--;
+            isLocked = true;
+            return true;
+        }
+        else
+        {
+            Debug.Log("You dont have right key!");
+            return false;
         }
     }
 }
