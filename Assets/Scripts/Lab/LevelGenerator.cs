30194fecdf05a6614ff97a22dcadb5452b7dc312
Wood <68517302+TinKolar@users.noreply.github.com>
2026-05-20 01:00:07 +0200

    part 1

--- Assets/Scripts/Lab/LevelGenerator.cs
+++ Assets/Scripts/Lab/LevelGenerator.cs
@@ -8,6 +8,8 @@ public class LevelGenerator : MonoBehaviour
     public ColorToPrefab[] colorMappings;
     public float offset = 5f;
 
+    public Material material01;
+    public Material material02;
     void GenerateTile(int x, int y)
     {
         Color pixelColor = map.GetPixel(x, y);
@@ -27,7 +29,7 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
-    public void GenerateLabyrinth()
+    public void GenerateLabirynth()
     {
         for(int x=0; x<map.width; x++)
         {
@@ -35,6 +37,46 @@ public class LevelGenerator : MonoBehaviour
             {
                 GenerateTile(x,z);
             }
+        }
+    }
+
+    public void ColorTheChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Wall")
+            {
+                if (Random.Range(1, 100) % 3 == 0)
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material02;
+                }
+                else
+            {
+                    child.gameObject.GetComponent<Renderer>().material = material01;
+                }
+            }
+            // Here you can add a piece of code
+            // which will check if the wall has any children and give them materials this way too
+            if (child.childCount > 0)
+            {
+                foreach (Transform grandchild in child.transform)
+                {
+                    if (grandchild.tag == "Wall")
+                    {
+                        if (Random.Range(1, 100) % 3 == 0)
+                        {
+                            grandchild.gameObject.GetComponent<Renderer>().material =
+                            material02;
+                        }
+                        else
+                    {
+                            grandchild.gameObject.GetComponent<Renderer>().material =
+                            material01;
+                        }
+                    }
+                }
+            }
+            // description below
         }
     }
 }
